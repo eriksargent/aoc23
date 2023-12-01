8ec951ec079beea682bceefcace35787e1d2bb13
Erik Sargent <admin@eriksargent.me>
2023-12-01 12:00:31 -0700

    Day 0

--- Sources/Day00.swift
+++ Sources/Day00.swift
@@ -1,25 +1,61 @@
 import Algorithms
+import Foundation
+
 
 struct Day00: AdventDay {
-  // Save your data in a corresponding text file in the `Data` directory.
-  var data: String
-
-  // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
-    }
-  }
-
-  // Replace this with your solution for the first part of the day's challenge.
-  func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
-  }
-
-  // Replace this with your solution for the second part of the day's challenge.
-  func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
-  }
+	// Save your data in a corresponding text file in the `Data` directory.
+	var data: String
+	
+	// Splits input data into its component parts and convert from string.
+	var entities: [Int] {
+		data
+			.components(separatedBy: .newlines).filter({ $0 != "" }).map {
+			let chars = $0.filter({ $0.isNumber })
+			return "\(chars.first!)\(chars.last!)"
+		}
+		.compactMap(Int.init)
+	}
+	
+	// Replace this with your solution for the first part of the day's challenge.
+	func part1() -> Any {
+		// Calculate the sum of the first set of input data
+		entities.reduce(0, +)
+	}
+	
+	// Replace this with your solution for the second part of the day's challenge.
+	func part2() -> Any {
+		var lines = data.components(separatedBy: .newlines).filter({ $0 != "" })
+		
+		do {
+			let regex = try Regex("(one|two|three|four|five|six|seven|eight|nine)")
+			let parser = NumberFormatter()
+			parser.numberStyle = .spellOut
+			lines = lines.map {
+				var line = $0
+				
+				var startIndex = line.startIndex
+				while let first = line[startIndex...].matches(of: regex).flatMap({ $0.output }).first, 
+						let value = first.substring,
+						let number = parser.number(from: String(value))?.intValue,
+						let range = first.range {
+					line.insert(contentsOf: "\(number)", at: range.lowerBound)
+					startIndex = line.index(range.lowerBound, offsetBy: 2)
+				}
+				
+				print("in: \($0) out: \(line)")
+				return line
+			}
+		}
+		catch let error {
+			print(error)
+		}
+		
+		let values = lines.map {
+			let chars = $0.filter({ $0.isNumber })
+			return "\(chars.first!)\(chars.last!)"
+		}
+		.compactMap(Int.init)
+		
+		return values.reduce(0, +)
+	}
 }
--- Tests/Day00.swift
+++ Tests/Day00.swift
@@ -5,32 +5,31 @@ import XCTest
 // Make a copy of this file for every day to ensure the provided smoke tests
 // pass.
 final class Day00Tests: XCTestCase {
-  // Smoke test data provided in the challenge question
-  let testData = """
-    1000
-    2000
-    3000
-
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-
-    """
-
-  func testPart1() throws {
-    let challenge = Day00(data: testData)
-    XCTAssertEqual(String(describing: challenge.part1()), "6000")
-  }
-
-  func testPart2() throws {
-    let challenge = Day00(data: testData)
-    XCTAssertEqual(String(describing: challenge.part2()), "32000")
-  }
+	// Smoke test data provided in the challenge question
+	let testData = """
+		 1abc2
+		 pqr3stu8vwx
+		 a1b2c3d4e5f
+		 treb7uchet
+		 """
+	
+	func testPart1() throws {
+		let challenge = Day00(data: testData)
+		XCTAssertEqual(String(describing: challenge.part1()), "142")
+	}
+	
+	let testData2 = """
+		two1nine
+		eightwothree
+		abcone2threexyz
+		xtwone3four
+		4nineeightseven2
+		zoneight234
+		7pqrstsixteen
+		"""
+	
+	func testPart2() throws {
+		let challenge = Day00(data: testData2)
+		XCTAssertEqual(String(describing: challenge.part2()), "281")
+	}
 }
